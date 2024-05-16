fffca9adafdf2db2545a368e026ee28ddd776d7f
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2024-05-16 22:26:36 +0200

    refactor: Update build information file generation script

--- app/scripts/updateBuildInfo.mjs
+++ app/scripts/updateBuildInfo.mjs
@@ -1,14 +1,14 @@
 import { writeFileSync } from 'fs';
 import { resolve } from 'path';
 
-const buildInfoPath = resolve('src/build.json');
+const buildInfoPath = resolve('src/assets/build.json');
 const packageJsonPath = resolve('package.json');
 
 import packageJson from '../package.json' assert { type: 'json' };
 
 const buildInfo = {
-  version: packageJson.version,
-  buildDate: new Date().toISOString(),
+	version: packageJson.version,
+	buildDate: new Date().toISOString(),
 };
 
 writeFileSync(buildInfoPath, JSON.stringify(buildInfo, null, 2));
