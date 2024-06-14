023cf70e80bc88af4f8ef6ff913b74bdda09d37d
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2024-06-14 06:59:42 +0000

    refactor: Add LoadingButton component for reusable loading button functionality

---
+++ app/src/components/LoadingButton.vue
@@ -0,0 +1,64 @@
+<template>
+	<ion-button @click="handleClick" :disabled="loading" expand="block" :class="{ loading: loading }">
+		<div class="button-content">
+			<ion-spinner v-if="loading" name="crescent" slot="start" />
+			<span>{{ label }}</span>
+		</div>
+	</ion-button>
+</template>
+
+<script setup lang="ts">
+	import { ref } from 'vue';
+	import { IonButton, IonSpinner } from '@ionic/vue';
+	
+	interface Props
+	{
+		label: string;
+		callback: () => Promise<void>;
+	}
+	
+	const props = defineProps<Props>();
+	const loading = ref(false);
+	
+	const handleClick = async () =>
+	{
+		loading.value = true;
+		try
+		{
+			await props.callback();
+		}
+		finally
+		{
+			loading.value = false;
+		}
+	};
+</script>
+
+<style scoped lang="scss">
+ion-button
+{
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	
+	.button-content
+	{
+		display: flex;
+		align-items: center;
+		
+		&> ion-spinner
+		{
+			margin-right: calc(var(--padding-start) * 0.5);
+			width: 1rem;
+			height: 1rem;
+			margin-right: 0.5rem;
+		}
+	}
+	
+	&.loading .button-content
+	{
+		min-width: 100%;
+		justify-content: center;
+	}
+}
+</style>
