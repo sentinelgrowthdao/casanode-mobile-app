938153040990ec75322b627ffd5da73e9ae6a1e3
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2024-06-16 19:36:22 +0200

    refactor: Update Step3Location to send node type and configuration to server before proceeding

--- app/src/views/Wizard/Step3Location.vue
+++ app/src/views/Wizard/Step3Location.vue
@@ -7,11 +7,18 @@ import {
 	IonSelect, IonSelectOption
 } from '@ionic/vue';
 import { useRouter } from 'vue-router';
+import { useI18n } from 'vue-i18n';
 import { useWizardStore } from '@stores/WizardStore';
+import BluetoothService from '@/services/BluetoothService';
 import LoadingButton from '@components/LoadingButton.vue';
 
-const wizardStore = useWizardStore();
+// Router
 const router = useRouter();
+// Import the useI18n composable function.
+const { t } = useI18n();
+
+const wizardStore = useWizardStore();
+const errorMessage: Ref<string> = ref('');
 const nodeType: Ref<string> = ref(wizardStore.nodeType);
 
 const setNodeTypeAndNavigate = async () =>
@@ -19,8 +26,17 @@ const setNodeTypeAndNavigate = async () =>
 	// Check if the node type is residential or datacenter
 	if(nodeType.value === 'residential' || nodeType.value === 'datacenter')
 	{
-		wizardStore.setNodeType(nodeType.value);
-		router.push({ name: 'Wizard4Protocol' });
+		// Send to the server and apply the value
+		if(await BluetoothService.writeNodeType(nodeType.value) && await BluetoothService.writeNodeConfig())
+		{
+			wizardStore.setNodeType(nodeType.value);
+			router.push({ name: 'Wizard4Protocol' });
+		}
+		else
+		{
+			// Show an error message
+			errorMessage.value = t('wizard.error-occurred') as string;
+		}
 	}
 };
 
