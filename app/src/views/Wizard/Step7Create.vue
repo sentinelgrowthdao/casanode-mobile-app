7f6d22e96e2dc1d08cbd2fd89dd074f27929f3a5
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2024-06-28 14:45:21 +0200

    feat: Update to use nodeStore.mnemonic for word list

--- app/src/views/Wizard/Step7Create.vue
+++ app/src/views/Wizard/Step7Create.vue
@@ -3,34 +3,10 @@ import {
 	IonPage, IonContent, IonButton,
 	IonGrid, IonCol, IonRow
 } from '@ionic/vue';
-import { onMounted } from 'vue';
-import { useRouter } from 'vue-router';
 import { useNodeStore } from '@/stores/NodeStore';
 
-// Router
-const router = useRouter();
 // Node store
 const nodeStore = useNodeStore();
-
-// On mounted
-onMounted(async () =>
-{
-	// Check if public address is already exist
-	if(nodeStore.publicAddress.length > 0)
-	{
-		// Navigate to the next step
-		router.replace({ name: 'Wizard8Fund' });
-	}
-});
-
-const mnemonic_words = [
-	"upper", "quarter", "grape", "wise",
-	"just", "used", "victory", "bird",
-	"evil", "setup", "ozone", "cat",
-	"assume", "carpet", "sing", "relief",
-	"surprise", "human", "blame", "this",
-	"dizzy", "save", "apology", "quiz",
-];
 
 </script>
 <template>
@@ -41,7 +17,7 @@ const mnemonic_words = [
 					<h1>{{ $t('wizard.wallet-create-title') }}</h1>
 					<p class="text">{{ $t('wizard.wallet-create-text') }}</p>
 					<ul class="mnemonic word-list">
-						<li v-for="(word, index) in mnemonic_words" :key="word">{{ index +1 }}. {{ word }}</li>
+						<li v-for="(word, index) in nodeStore.mnemonic" :key="word">{{ index +1 }}. {{ word }}</li>
 					</ul>
 					<div class="backup-alert">
 						<h2>{{ $t('wizard.wallet-create-backup-title') }}</h2>
