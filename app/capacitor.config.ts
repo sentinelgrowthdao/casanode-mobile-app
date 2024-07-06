d047e03709982898c8ede3ca980d50effab5113b
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2024-07-07 01:09:56 +0200

    refactor: Update webDir to 'dist' in capacitor.config.ts

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
 	appId: 'com.casanode.app',
 	appName: 'casanode-app',
-	webDir: 'public'
+	webDir: 'dist'
 };
 
 export default config;
