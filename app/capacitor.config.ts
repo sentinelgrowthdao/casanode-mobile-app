4f5ba79d114b3d8f2100233f0f73678706115ec8
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2024-07-07 23:28:25 +0200

    refactor: Update app ID in capacitor.config.ts

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-	appId: 'com.sentinel.casanode',
+	appId: 'co.sentinel.casanode',
 	appName: 'Casanode',
 	webDir: 'dist'
 };
