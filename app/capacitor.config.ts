535165f9ae93f105c9843a8387081c4b27648b8c
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2024-07-29 22:14:18 +0200

    feat: Add Capacitor plugins for MLKit barcode scanning and Bluetooth LE

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
 	appId: 'co.sentinel.casanode',
 	appName: 'Casanode',
-	webDir: 'dist'
+	webDir: 'dist',
+	plugins:
+	{
+		CapacitorMLKitBarcodeScanning: {
+			cameraPermissionText: "We need camera access to scan QR codes."
+		},
+		BluetoothLe: {
+			bluetoothPermissionText: "We need Bluetooth access to connect to your device."
+		}
+	}
 };
 
 export default config;
