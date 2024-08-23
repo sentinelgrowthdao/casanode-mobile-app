af864c59645508870d2b34dabd472148d4ff58c5
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2024-08-24 00:42:40 +0200

    feat: added css style for login help page

--- app/src/components/ConnectHelpModal.vue
+++ app/src/components/ConnectHelpModal.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { defineEmits } from 'vue';
-import { IonContent, IonButton, modalController } from '@ionic/vue';
+import {
+	IonContent, IonButton,
+	modalController
+} from '@ionic/vue';
 
 const emit = defineEmits(['dismiss']);
 
@@ -11,15 +14,51 @@ const dismissModal = () =>
 };
 </script>
 <template>
-	<ion-content>
+	<ion-content :fullscreen="true" class="ion-padding">
 		<h1>{{ $t('welcome.help-title') }}</h1>
-		<p>{{ $t('welcome.help-text') }}</p>
-		<p>http://adresse-ip:8080</p>
-		<p>
-			<ion-button @click="dismissModal">Close</ion-button>
+		<p class="text">{{ $t('welcome.help-text') }}</p>
+		<p class="text">{{ $t('welcome.help-instruction') }}</p>
+		<p class="picture"><img src="@assets/images/browser.svg" alt="" /></p>
+		<p class="close">
+			<ion-button @click="dismissModal">{{ $t('welcome.help-close') }}</ion-button>
 		</p>
 	</ion-content>
 </template>
 <style scoped lang="scss">
+ion-content
+{
+	&> h1
+	{
+		font-size:2.5rem;
+	}
+	
+	&> p
+	{
+		&.text
+		{
+			margin:0 0 1rem;
+			font-size:1.125rem;
+			line-height:1.5rem;
+		}
+		
+		&.picture
+		{
+			margin:0 0 1rem;
+			padding: 0 2rem;
+			text-align:center;
+			
+			&> img
+			{
+				display:inline-block;
+				width:100%;
+			}
+		}
 
+		&.close
+		{
+			margin:0;
+			text-align:right;
+		}
+	}
+}
 </style>
