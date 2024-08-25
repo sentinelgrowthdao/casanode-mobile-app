ae1c9b0a571e41d67cbec627fde82ed59acfe445
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2024-08-25 15:23:16 +0200

    chore: Remove unused import in BluetoothMonitorHook.ts

--- app/src/hooks/BluetoothMonitorHook.ts
+++ app/src/hooks/BluetoothMonitorHook.ts
@@ -1,4 +1,4 @@
-import { onMounted, onUnmounted, ref } from 'vue';
+import { onMounted, onUnmounted } from 'vue';
 import { App } from '@capacitor/app';
 import { useRouter } from 'vue-router';
 import { requiresBluetooth } from '@/router';
